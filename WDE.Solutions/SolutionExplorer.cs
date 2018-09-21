bb532628c16d5e6e0b406aa958e616c7bad46bbc
bandysc <b.andysc@gmail.com>
2018-09-21 23:29:36 +0200

    [Solutions] Removed now unnecessary proxy class

--- WDE.Solutions/SolutionExplorer.cs
+++ WDE.Solutions/SolutionExplorer.cs
@@ -5,16 +5,27 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
 using WDE.Common;
+using WDE.Common.Attributes;
+using WDE.Common.Windows;
 using WDE.Solutions.Explorer.Views;
 
 namespace WDE.Solutions
 {
-    [WDE.Common.Attributes.AutoRegister]
-    public class SolutionExplorer : ISolutionExplorer
+    [AutoRegister, SingleInstance]
+    public class SolutionExplorer : ISolutionExplorer, IToolProvider
     {
+        public bool AllowMultiple => false;
+
+        public string Name => "Solution explorer";
+
         public ContentControl GetSolutionExplorerView()
         {
             return new SolutionExplorerView();
+        }
+
+        public ContentControl GetView()
+        {
+            return GetSolutionExplorerView();
         }
     }
 }
