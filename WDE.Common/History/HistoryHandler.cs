2ca875d9a5948b92dff9cba8e039c0ed67d4880f
BAndysc <b.andysc@gmail.com>
2021-01-07 01:58:13 +0000

    [History] Bulk history action

--- WDE.Common/History/HistoryHandler.cs
+++ WDE.Common/History/HistoryHandler.cs
@@ -9,9 +9,31 @@ namespace WDE.Common.History
     {
         public event EventHandler<IHistoryAction> ActionPush = delegate { };
 
+        private List<IHistoryAction> bulkEditing = new List<IHistoryAction>();
+        private bool inBulkEditing = false;
+        
+        protected void StartBulkEdit()
+        {
+            inBulkEditing = true;
+            bulkEditing.Clear();
+        }
+
+        protected void EndBulkEdit(string name)
+        {
+            if (inBulkEditing)
+            {
+                inBulkEditing = false;
+                if (bulkEditing.Count > 0)
+                    PushAction(new CompoundHistoryAction(name, bulkEditing.ToArray()));
+            }
+        }
+
         protected void PushAction(IHistoryAction action)
         {
-            ActionPush(this, action);
+            if (inBulkEditing)
+                bulkEditing.Add(action);
+            else
+                ActionPush(this, action);
         }
     }
 }
