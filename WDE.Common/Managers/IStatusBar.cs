b6210523390c7f45656c26199409ce296f58859d
BAndysc <b.andysc@gmail.com>
2021-01-11 01:28:07 +0000

    [Statusbar] Statusbar

---
+++ WDE.Common/Managers/IStatusBar.cs
@@ -0,0 +1,36 @@
+﻿using WDE.Module.Attributes;
+
+namespace WDE.Common.Managers
+{
+    [UniqueProvider]
+    public interface IStatusBar
+    {
+        void PublishNotification(INotification notification);
+    }
+
+    public interface INotification
+    {
+        NotificationType Type { get; }
+        string Message { get; }
+    }
+
+    public struct PlainNotification : INotification
+    {
+        public PlainNotification(NotificationType type, string message)
+        {
+            Type = type;
+            Message = message;
+        }
+
+        public NotificationType Type { get; }
+        public string Message { get; }
+    }
+
+    public enum NotificationType
+    {
+        Info,
+        Warning,
+        Success,
+        Error
+    }
+}
