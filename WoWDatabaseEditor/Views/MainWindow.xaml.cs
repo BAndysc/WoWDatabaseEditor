5317bd8ea7d853d6c16dea06da3d4bb15b5e1c90
BAndysc <b.andysc@gmail.com>
2021-01-13 03:44:14 +0000

    [Shell] Fixed slow as hell resizing window
    It was due to constantly saving and loading layout, which in fact didn't even work

--- WoWDatabaseEditor/Views/MainWindow.xaml.cs
+++ WoWDatabaseEditor/Views/MainWindow.xaml.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Input;
 using AvalonDock;
-using AvalonDock.Layout;
-using AvalonDock.Layout.Serialization;
 using MahApps.Metro.Controls;
 
 namespace WoWDatabaseEditor.Views
@@ -30,34 +27,6 @@ namespace WoWDatabaseEditor.Views
         private void DockingManager_OnDocumentClosed(object sender, DocumentClosedEventArgs e)
         {
             DocumentClosedCommand?.Execute(e.Document.Content);
-        }
-
-        private void Window_Closed(object sender, EventArgs e)
-        {
-            XmlLayoutSerializer? layoutSerializer = new(DockingManager);
-            layoutSerializer.Serialize(@".\AvalonDock.Layout.config");
-        }
-
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            LoadLayout();
-        }
-
-        public void LoadLayout()
-        {
-            XmlLayoutSerializer? layoutSerializer = new(DockingManager);
-            layoutSerializer.LayoutSerializationCallback += (s, e) =>
-            {
-                LayoutContent? x = e.Model;
-                e.Cancel = true;
-            };
-            try
-            {
-                layoutSerializer.Deserialize(@".\AvalonDock.Layout.config");
-            }
-            catch (Exception)
-            {
-            }
         }
     }
 }
