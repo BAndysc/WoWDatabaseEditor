7345ef7d25e76022a2bec54f4049691a5f8ac059
BAndysc <b.andysc@gmail.com>
2021-01-17 00:39:05 +0000

    [Blueprints] Disable blueprints in new item window
    It was only an experiment

--- WDE.Blueprints/Providers/BlueprintItemProvider.cs
+++ WDE.Blueprints/Providers/BlueprintItemProvider.cs
@@ -6,7 +6,7 @@ using WDE.Module.Attributes;
 
 namespace WDE.Blueprints.Providers
 {
-    [AutoRegister]
+    //[AutoRegister]
     public class BlueprintItemProvider : ISolutionItemProvider
     {
         public ISolutionItem CreateSolutionItem()
