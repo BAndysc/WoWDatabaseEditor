6af92cc7d5ef8f6da7ff89334887c6aad1a53669
BAndysc <b.andysc@gmail.com>
2021-03-01 02:12:38 +0000

    [UX/SmartScript] Fixed bug with pressing enter in search box in smartscripts

---
+++ WDE.SmartScriptEditor.Avalonia/Editor/UserControls/FixedTextBox.cs
@@ -0,0 +1,22 @@
+using System;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Styling;
+
+namespace WDE.SmartScriptEditor.Avalonia.Editor.UserControls
+{
+    // normal TextBox has bug when AcceptsReturn is false
+    public class FixedTextBox : TextBox, IStyleable
+    {
+        Type IStyleable.StyleKey => typeof(TextBox);
+        
+        protected override void OnTextInput(TextInputEventArgs e)
+        {
+            if (!e.Handled)
+            {
+                if (AcceptsReturn || (e.Text != "\r" && e.Text != "\n"))
+                    base.OnTextInput(e);
+            }
+        }
+    }
+}
