89d6be6ff59a484cd2bba2937ef90db287f52e3d
BAndysc <b.andysc@gmail.com>
2021-03-08 18:24:44 +0000

    [Core] Added IUserSettings to handle user preferences

---
+++ WoWDatabaseEditor/Services/UserSettingsService/UserSettings.cs
@@ -0,0 +1,67 @@
+using System;
+using System.IO;
+using Newtonsoft.Json;
+using WDE.Common.Managers;
+using WDE.Common.Services;
+using WDE.Module.Attributes;
+
+namespace WoWDatabaseEditorCore.Services.UserSettingsService
+{
+    [SingleInstance]
+    [AutoRegister]
+    public class UserSettings : IUserSettings
+    {
+        private readonly IFileSystem fileSystem;
+        private readonly IStatusBar statusBar;
+        private readonly string basePath;
+        private readonly JsonSerializer json;
+        
+        public UserSettings(IFileSystem fileSystem, IStatusBar statusBar)
+        {
+            json = new() {TypeNameHandling = TypeNameHandling.Auto, Formatting = Formatting.Indented};
+            this.fileSystem = fileSystem;
+            this.statusBar = statusBar;
+            basePath = "~";
+        }
+        
+        private string GetSettingPath<T>()
+        {
+            return Path.Join(basePath, typeof(T).FullName!.Replace("+", ".") + ".json");
+        }
+        
+        public T? Get<T>(T? defaultValue = default) where T : ISettings
+        {
+            var settingsFile = GetSettingPath<T>();
+            if (!fileSystem.Exists(settingsFile))
+                return defaultValue;
+            
+            try
+            {
+                using var file = fileSystem.OpenRead(settingsFile);
+                using var stream = new StreamReader(file);
+                using var reader = new JsonTextReader(stream);
+                return json.Deserialize<T>(reader);
+            }
+            catch (Exception e)
+            {
+                statusBar.PublishNotification(new PlainNotification(NotificationType.Error, "Error while loading settings: " + e));
+                return defaultValue;
+            }
+        }
+
+        public void Update<T>(T newSettings) where T : ISettings
+        {
+            var settingsFile = GetSettingPath<T>();
+            try
+            {
+                using var writer = fileSystem.OpenWrite(settingsFile);
+                using var streamWriter = new StreamWriter(writer);
+                json.Serialize(streamWriter, newSettings);
+            }
+            catch (Exception e)
+            {
+                statusBar.PublishNotification(new PlainNotification(NotificationType.Error, "Error while saving settings: " + e));
+            }
+        }
+    }
+}
