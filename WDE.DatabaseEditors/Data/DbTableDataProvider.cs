9367436293c41e818ef565ccb7317923fd57ad02
Pelekon <pelekon@gmail.com>
2021-03-13 23:24:19 +0100

    A little better error handling.

--- WDE.DatabaseEditors/Data/DbTableDataProvider.cs
+++ WDE.DatabaseEditors/Data/DbTableDataProvider.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using WDE.Common.Services.MessageBox;
 using WDE.DatabaseEditors.Models;
 using WDE.Module.Attributes;
 
@@ -10,10 +11,12 @@ namespace WDE.DatabaseEditors.Data
     public class DbTableDataProvider : IDbTableDataProvider
     {
         private readonly Lazy<IDbFieldNameSwapDataManager> nameSwapDataManager;
+        private readonly Lazy<IMessageBoxService> messageBoxService;
 
-        public DbTableDataProvider(Lazy<IDbFieldNameSwapDataManager> nameSwapDataManager)
+        public DbTableDataProvider(Lazy<IDbFieldNameSwapDataManager> nameSwapDataManager, Lazy<IMessageBoxService> messageBoxService)
         {
             this.nameSwapDataManager = nameSwapDataManager;
+            this.messageBoxService = messageBoxService;
         }
         
         public IDbTableData GetDatabaseTable(in DatabaseEditorTableDefinitionJson tableDefinition, IDbTableFieldFactory fieldFactory, Dictionary<string, object> fieldsFromDb)
@@ -28,11 +31,24 @@ namespace WDE.DatabaseEditors.Data
                 : "";
             var tableDesc = $"Template of {descNameField ?? "Unk"}";
 
-            foreach (var category in tableDefinition.Groups)
-                tableCategories.Add(CreateCategory(in category, fieldFactory, fieldsFromDb));
+            try
+            {
+                foreach (var category in tableDefinition.Groups)
+                    tableCategories.Add(CreateCategory(in category, fieldFactory, fieldsFromDb));
 
-            return new DbTableData(tableDefinition.Name, tableDefinition.TableName, tableDefinition.TablePrimaryKeyColumnName, 
-                tableIndex ?? "Unk", tableDesc, tableCategories);
+                return new DbTableData(tableDefinition.Name, tableDefinition.TableName, tableDefinition.TablePrimaryKeyColumnName, 
+                    tableIndex ?? "Unk", tableDesc, tableCategories);
+            }
+            catch (Exception e)
+            {
+                messageBoxService.Value.ShowDialog(new MessageBoxFactory<bool>().SetTitle("Error!")
+                    .SetMainInstruction(e.Message)
+                    .SetIcon(MessageBoxIcon.Error)
+                    .WithOkButton(true)
+                    .Build());
+            }
+            
+            return null;
         }
 
         private DbTableColumnCategory CreateCategory(in DbEditorTableGroupJson groupDefinition, IDbTableFieldFactory fieldFactory, Dictionary<string, object> fieldsFromDb)
