78f8811953247384919568ebd9454346c6b2228e
Pelekon <pelekon@gmail.com>
2021-03-14 14:34:35 +0100

    Add generic way to input entry (e.g entry value for table to load).

---
+++ WoWDatabaseEditor/Services/InputEntryProviderService/InputEntryProvider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using WDE.Common.Managers;
+using WDE.Common.Providers;
+using WDE.Module.Attributes;
+
+namespace WoWDatabaseEditorCore.Services.InputEntryProviderService
+{
+    [AutoRegister]
+    public class InputEntryProvider : IInputEntryProvider
+    {
+        private readonly Lazy<IWindowManager> windowManager;
+        
+        public InputEntryProvider(Lazy<IWindowManager> windowManager)
+        {
+            this.windowManager = windowManager;
+        }
+        
+        public async Task<uint?> GetEntry()
+        {
+            var vm = new InputEntryProviderViewModel();
+            if (await windowManager.Value.ShowDialog(vm))
+                return vm.Entry;
+            return null;
+        }
+    }
+}
