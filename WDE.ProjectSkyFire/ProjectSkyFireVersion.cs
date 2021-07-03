9294efe22bf5c61bdbbb898fe46a1065ff0a627a
Crypticaz <nick.lop6@gmail.com>
2021-07-03 14:55:16 -0500

    - Start of ProjectSkyFire implementation

---
+++ WDE.ProjectSkyFire/ProjectSkyFireVersion.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using WDE.Common.CoreVersion;
+using WDE.Common.Database;
+using WDE.Module.Attributes;
+
+namespace WDE.ProjectSkyFire
+{
+    [AutoRegister]
+    [SingleInstance]
+    public class ProjectSkyFireVersion : ICoreVersion, IDatabaseFeatures, ISmartScriptFeatures, IConditionFeatures
+    {
+        public string Tag => "SkyFire";
+        public string FriendlyName => "ProjectSkyFire Mists of Pandaria";
+        public ISmartScriptFeatures SmartScriptFeatures => this;
+        public IConditionFeatures ConditionFeatures => this;
+        public IDatabaseFeatures DatabaseFeatures => this;
+
+        public ISet<Type> UnsupportedTables { get; } = new HashSet<Type>{typeof(IAreaTriggerTemplate),
+            typeof(IConversationTemplate),
+            typeof(IAuthRbacPermission),
+            typeof(IAuthRbacLinkedPermission)
+        };
+        
+        public ISet<SmartScriptType> SupportedTypes { get; } = new HashSet<SmartScriptType>
+        {
+            SmartScriptType.Creature,
+            SmartScriptType.GameObject,
+            SmartScriptType.AreaTrigger,
+            SmartScriptType.TimedActionList,
+        };
+
+        public string ConditionsFile => "SmartData/conditions.json";
+        public string ConditionGroupsFile => "SmartData/conditions_groups.json";
+        public string ConditionSourcesFile => "SmartData/condition_sources.json";
+    }
+}
