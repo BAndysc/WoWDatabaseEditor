9937a618797e003db2ac8e73e2a0cedf0b034247
Bartosz Korczyński <b.andysc@gmail.com>
2021-07-06 13:01:18 +0100

    [UX] Fixed displaying messageboxes before window is loaded

--- WoWDatabaseEditorCore.Avalonia/Managers/MessageBoxService.cs
+++ WoWDatabaseEditorCore.Avalonia/Managers/MessageBoxService.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using Prism.Events;
+using WDE.Common.Events;
 using WDE.Common.Managers;
 using WDE.Common.Services.MessageBox;
 using WDE.Common.Windows;
@@ -14,19 +17,54 @@ namespace WoWDatabaseEditorCore.Avalonia.Managers
     public class MessageBoxService : IMessageBoxService
     {
         private readonly IMainWindowHolder mainWindowHolder;
+        private bool loaded;
+        private List<Func<Task>> pending = new();
 
-        public MessageBoxService(IMainWindowHolder mainWindowHolder)
+        public MessageBoxService(IMainWindowHolder mainWindowHolder,
+            IEventAggregator eventAggregator)
         {
             this.mainWindowHolder = mainWindowHolder;
+            eventAggregator.GetEvent<AllModulesLoaded>()
+                .Subscribe(OnLoaded, true);
         }
-        
-        public async Task<T?> ShowDialog<T>(IMessageBox<T> messageBox)
+
+        private void OnLoaded()
+        {
+            loaded = true;
+            ExecutePending();
+        }
+
+        private async Task ExecutePending()
+        {
+            while (pending.Count > 0)
+            {
+                await pending[0]();
+                pending.RemoveAt(0);
+            }
+        }
+
+        private async Task<T?> ShowNow<T>(IMessageBox<T> messageBox)
         {
             MessageBoxView view = new MessageBoxView();
             var viewModel = new MessageBoxViewModel<T>(messageBox);
             view.DataContext = viewModel;
             await view.ShowDialog<bool>(mainWindowHolder.Window);
             return viewModel.SelectedOption;
+        }
+        
+        public Task<T?> ShowDialog<T>(IMessageBox<T> messageBox)
+        {
+            if (loaded)
+                return ShowNow(messageBox);
+            
+            TaskCompletionSource<T?> completionSource = new();
+            Func<Task> f = async () =>
+            {
+                var result = await ShowNow(messageBox);
+                completionSource.SetResult(result);
+            };
+            pending.Add(f);
+            return completionSource.Task;
         }
     }
 }
