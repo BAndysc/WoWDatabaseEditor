5e31c870a0f062731f6e7eb64fa628964421b6a2
Bartosz Korczyński <b.andysc@gmail.com>
2021-09-07 15:06:11 +0200

    [Packet viewer] Added compound processors

--- WDE.PacketViewer/Processing/Runners/CompoundProcessor.cs
+++ WDE.PacketViewer/Processing/Runners/CompoundProcessor.cs
@@ -3,6 +3,22 @@ using WowPacketParser.Proto.Processing;
 
 namespace WDE.PacketViewer.Processing.Runners
 {
+    public abstract class CompoundProcessor<T, R1> : PacketProcessor<T>, ITwoStepPacketProcessor<bool> where R1 : IPacketProcessor<T>
+    {
+        private readonly R1 r1;
+
+        protected CompoundProcessor(R1 r1)
+        {
+            this.r1 = r1;
+        }
+
+        public bool PreProcess(PacketHolder packet)
+        {
+            r1.Process(packet);
+            return true;
+        }
+    }
+    
     public abstract class CompoundProcessor<T, R1, R2> : PacketProcessor<T>, ITwoStepPacketProcessor<bool> where R1 : IPacketProcessor<T> where R2 : IPacketProcessor<T>
     {
         private readonly R1 r1;
@@ -18,6 +34,29 @@ namespace WDE.PacketViewer.Processing.Runners
         {
             r1.Process(packet);
             r2.Process(packet);
+            return true;
+        }
+    }
+    public abstract class CompoundProcessor<T, R1, R2, R3> : PacketProcessor<T>, ITwoStepPacketProcessor<bool> where R1 : IPacketProcessor<T> 
+        where R2 : IPacketProcessor<T>
+        where R3 : IPacketProcessor<T>
+    {
+        private readonly R1 r1;
+        private readonly R2 r2;
+        private readonly R3 r3;
+
+        protected CompoundProcessor(R1 r1, R2 r2, R3 r3)
+        {
+            this.r1 = r1;
+            this.r2 = r2;
+            this.r3 = r3;
+        }
+
+        public bool PreProcess(PacketHolder packet)
+        {
+            r1.Process(packet);
+            r2.Process(packet);
+            r3.Process(packet);
             return true;
         }
     }
