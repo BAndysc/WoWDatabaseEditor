ff56ca46ce27d46cb71ebfe464bab2d260e460a8
Bartosz Korczyński <b.andysc@gmail.com>
2021-09-20 10:57:10 +0100

    [UX] Styling a tab control

--- WDE.Common.Avalonia/Converters/CommonConverters.cs
+++ WDE.Common.Avalonia/Converters/CommonConverters.cs
@@ -3,5 +3,7 @@ namespace WDE.Common.Avalonia.Converters
     public static class CommonConverters
     {
         public static LongToBoolConverter LongToBoolConverter { get; } = new();
+        public static NullConverter NullToBoolConverter { get; } = new();
+        public static NullConverter InversedNullToBoolConverter { get; } = new(){Inverted = true};
     }
 }
