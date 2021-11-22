6f16d538c8aa1bb6af0b2d190d6b425dfcccca80
Bartosz Korczyński <b.andysc@gmail.com>
2021-11-22 22:59:44 +0000

    Fix LoaderAvalonia startup project

--- WoWDatabaseEditorCore.Avalonia/Program.cs
+++ WoWDatabaseEditorCore.Avalonia/Program.cs
@@ -15,7 +15,7 @@ using WoWDatabaseEditorCore.Managers;
 
 namespace WoWDatabaseEditorCore.Avalonia
 {
-    class Program
+    public class Program
     {
         // Initialization code. Don't use any Avalonia, third-party APIs or any
         // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
