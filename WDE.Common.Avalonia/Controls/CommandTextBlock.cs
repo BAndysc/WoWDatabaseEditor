2df0c02b2e78649353dbaa453ff9741e53cade14
Bartosz Korczyński <b.andysc@gmail.com>
2021-12-02 18:59:41 +0000

    [UX] ImageToggleButton, CommandTextBlock

---
+++ WDE.Common.Avalonia/Controls/CommandTextBlock.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Windows.Input;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Styling;
+
+namespace WDE.Common.Avalonia.Controls;
+
+public class CommandTextBlock : TextBlock, IStyleable
+{
+    public static readonly StyledProperty<object?> CommandParameterProperty = AvaloniaProperty.Register<CommandTextBlock, object?>("CommandParameter");
+    public static readonly StyledProperty<ICommand?> CommandProperty = AvaloniaProperty.Register<CommandTextBlock, ICommand?>("Command");
+    Type IStyleable.StyleKey => typeof(TextBlock);
+
+    public object? CommandParameter
+    {
+        get => (object?)GetValue(CommandParameterProperty);
+        set => SetValue(CommandParameterProperty, value);
+    }
+
+    public ICommand? Command
+    {
+        get => (ICommand?)GetValue(CommandProperty);
+        set => SetValue(CommandProperty, value);
+    }
+
+    protected override void OnPointerPressed(PointerPressedEventArgs e)
+    {
+        if (Command != null)
+        {
+            Command.Execute(CommandParameter);
+            e.Handled = true;
+        }
+        else
+            base.OnPointerPressed(e);
+    }
+}
