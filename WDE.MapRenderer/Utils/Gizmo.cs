8acc6ee12f9b877c3b3742fcd1917e34978820c3
Bartosz Korczyński <b.andysc@gmail.com>
2021-12-19 18:59:51 +0000

    [3D] Gizmo drawing fix

--- WDE.MapRenderer/Utils/Gizmo.cs
+++ WDE.MapRenderer/Utils/Gizmo.cs
@@ -21,13 +21,13 @@ namespace WDE.MapRenderer.Utils
             this.material = material;
         }
 
-        private static readonly Quaternion ArrowX = Quaternion.FromEuler(0, 180, 0);
-        private static readonly Quaternion ArrowY = Quaternion.FromEuler(0, 90, 0);
-        private static readonly Quaternion ArrowZ = Quaternion.FromEuler(90, 90, 0);
+        private static readonly Quaternion ArrowX = Quaternion.LookRotation(Vector3.Left, Vector3.Up);
+        private static readonly Quaternion ArrowY = Quaternion.LookRotation(Vector3.Forward, Vector3.Up);
+        private static readonly Quaternion ArrowZ = Quaternion.FromEuler(0, 0, -90);
         
-        private static readonly Quaternion PlaneX = Quaternion.FromEuler(180, 180, 180);
-        private static readonly Quaternion PlaneY = Quaternion.FromEuler(0, 90, 0);
-        private static readonly Quaternion PlaneZ = Quaternion.FromEuler(90, 90, 180);
+        private static readonly Quaternion PlaneX = Quaternion.LookRotation(Vector3.Forward, Vector3.Up);
+        private static readonly Quaternion PlaneY = Quaternion.LookRotation(Vector3.Left, Vector3.Up);
+        private static readonly Quaternion PlaneZ = Quaternion.LookRotation(Vector3.Up, Vector3.Up);
 
         // wow direction hit test
         public enum HitType
