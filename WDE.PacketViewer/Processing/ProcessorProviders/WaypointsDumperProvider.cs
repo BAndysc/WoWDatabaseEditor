98ccd356b9e8a69edae4255a41c5cabb1a60d45f
Bartosz Korczyński <b.andysc@gmail.com>
2022-04-13 23:55:06 +0200

    [Packet parser] Colorize waypoints packet parser output

--- WDE.PacketViewer/Processing/ProcessorProviders/WaypointsDumperProvider.cs
+++ WDE.PacketViewer/Processing/ProcessorProviders/WaypointsDumperProvider.cs
@@ -14,7 +14,7 @@ namespace WDE.PacketViewer.Processing.ProcessorProviders
         }
         public string Name => "Creature waypoints dump";
         public string Description => "Generate all waypoints per each unit in sniff [ALPHA]";
-        public string Extension => "txt";
+        public string Extension => "waypoints";
         public ImageUri? Image { get; } = new ImageUri("Icons/document_waypoints_big.png");
         public Task<IPacketTextDumper> CreateDumper() =>
             Task.FromResult<IPacketTextDumper>(new WaypointsToTextProcessor(new WaypointsProcessor()));
