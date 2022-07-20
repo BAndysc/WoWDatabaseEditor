2c25d3ab103e9ca5310aa005ba74a780f221797f
Bartosz Korczyński <b.andysc@gmail.com>
2022-07-20 22:58:03 +0200

    [UX] Move Table editor search bar to the toolbar

--- WDE.DatabaseEditors.Avalonia/Views/Template/TemplateDbTableEditorToolBar.axaml.cs
+++ WDE.DatabaseEditors.Avalonia/Views/Template/TemplateDbTableEditorToolBar.axaml.cs
@@ -1,13 +1,54 @@
+using System;
+using System.Windows.Input;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Input.Platform;
 using Avalonia.Markup.Xaml;
+using Avalonia.VisualTree;
+using Prism.Commands;
+using WDE.Common.Utils;
 
 namespace WDE.DatabaseEditors.Avalonia.Views.Template
 {
     public class TemplateDbTableEditorToolBar : UserControl
     {
+        private ICommand focusCommand;
+        private Window? attachedRoot = null;
+        
         public TemplateDbTableEditorToolBar()
         {
             InitializeComponent();
+            focusCommand = new DelegateCommand(() =>
+            {
+                TextBox tb = this.FindControl<TextBox>("SearchTextBox");
+                tb?.Focus();
+            });
+        }
+
+        protected override void OnAttachedToVisualTree(VisualTreeAttachmentEventArgs e)
+        {
+            base.OnAttachedToVisualTree(e);
+            attachedRoot = this.GetVisualRoot() as Window;
+            if (attachedRoot != null)
+            {
+                attachedRoot.KeyBindings.Add(new KeyBinding()
+                    {
+                        Command = focusCommand,
+                        Gesture = new KeyGesture(Key.F, AvaloniaLocator.Current
+                            .GetService<PlatformHotkeyConfiguration>()?.CommandModifiers ?? KeyModifiers.Control)
+                    });
+            }
+        }
+
+        protected override void OnDetachedFromVisualTree(VisualTreeAttachmentEventArgs e)
+        {
+            base.OnDetachedFromVisualTree(e);
+            if (attachedRoot != null)
+            {
+                attachedRoot.KeyBindings.RemoveIf(x => x.Command == focusCommand);
+                attachedRoot = null;
+            }
         }
 
         private void InitializeComponent()
--- WDE.DatabaseEditors.Avalonia/Views/Template/TemplateDbTableEditorView.axaml.cs
+++ WDE.DatabaseEditors.Avalonia/Views/Template/TemplateDbTableEditorView.axaml.cs
@@ -13,41 +13,11 @@ namespace WDE.DatabaseEditors.Avalonia.Views.Template
         public TemplateDbTableEditorView()
         {
             InitializeComponent();
-            KeyBindings.Add(new KeyBinding()
-            {
-                Command = new DelegateCommand(() =>
-                {
-                    TextBox tb = this.FindControl<TextBox>("SearchTextBox") as TextBox;
-                    tb?.Focus();
-                }),
-                Gesture = new KeyGesture(Key.F, AvaloniaLocator.Current
-                    .GetService<PlatformHotkeyConfiguration>()?.CommandModifiers ?? KeyModifiers.Control)
-            });
         }
 
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
-        }
-
-        protected override void OnDetachedFromVisualTree(VisualTreeAttachmentEventArgs e)
-        {
-            base.OnDetachedFromVisualTree(e);
-        }
-
-        protected override void OnDetachedFromLogicalTree(LogicalTreeAttachmentEventArgs e)
-        {
-            base.OnDetachedFromLogicalTree(e);
-        }
-
-        protected override void OnAttachedToVisualTree(VisualTreeAttachmentEventArgs e)
-        {
-            base.OnAttachedToVisualTree(e);
-        }
-
-        protected override void OnAttachedToLogicalTree(LogicalTreeAttachmentEventArgs e)
-        {
-            base.OnAttachedToLogicalTree(e);
         }
     }
 }
