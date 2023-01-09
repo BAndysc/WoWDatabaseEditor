be268f616dc0e306a3b60aa55b965fb0b7b3e7b6
Bartosz Korczyński <b.andysc@gmail.com>
2023-01-09 04:01:03 +0100

    [SmartScript] Fixed a rare bug when typing a number inside a textbox always typed number '1'

---
+++ WDE.Common.Avalonia/Utils/Fix.cs
@@ -0,0 +1,34 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.LogicalTree;
+using WDE.MVVM.Observable;
+
+namespace WDE.Common.Avalonia.Utils;
+
+public static class Fix
+{
+    /// <summary>
+    /// This is a workaround for avalonia bug https://github.com/AvaloniaUI/Avalonia/issues/9940
+    /// </summary>
+    public static readonly AvaloniaProperty DetachDataContextProperty = AvaloniaProperty.RegisterAttached<Control, bool>("DetachDataContext", typeof(Fix));
+        
+    public static bool GetDetachDataContext(Control control) => (bool)control.GetValue(DetachDataContextProperty);
+    public static void SetDetachDataContext(Control control, bool value) => control.SetValue(DetachDataContextProperty, value);
+
+    static Fix()
+    {
+        DetachDataContextProperty.Changed.SubscribeAction(args =>
+        {
+            if (args.Sender is Control c)
+            {
+                c.DetachedFromLogicalTree += OnDetachedFromLogicalTree;
+            }
+        });
+    }
+
+    private static void OnDetachedFromLogicalTree(object? sender, LogicalTreeAttachmentEventArgs e)
+    {
+        if (sender is Control c)
+            c.DataContext = null;
+    }
+}
