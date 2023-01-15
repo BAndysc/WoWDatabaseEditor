f92261638802f40e92588d221a53f345504743cc
Bartosz Korczyński <b.andysc@gmail.com>
2023-01-16 00:44:44 +0100

    [UX] Hide unused parameter

--- WDE.Parameters/ParameterLoader.cs
+++ WDE.Parameters/ParameterLoader.cs
@@ -92,7 +92,7 @@ namespace WDE.Parameters
             }
 
             factory.Register("InvalidParameter", new InvalidParameter<long>());
-            factory.Register("UnusedParameter", new UnusedParameter());
+            factory.Register("UnusedParameter", UnusedParameter.Instance);
             factory.Register("FloatParameter", new FloatIntParameter(1000));
             factory.Register("DecifloatParameter", new FloatIntParameter(100));
             factory.Register("GameEventParameter", AddDatabaseParameter(new GameEventParameter(database)), QuickAccessMode.Limited);
