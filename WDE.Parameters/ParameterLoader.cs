f281c3b9e7c09311c040937bbb241605d1d375c6
Bartosz Korczyński <b.andysc@gmail.com>
2023-09-08 01:03:38 +0100

    [Parameters] Fix PlayerChoiceResponseParameter ToString

--- WDE.Parameters/ParameterLoader.cs
+++ WDE.Parameters/ParameterLoader.cs
@@ -790,7 +790,7 @@ namespace WDE.Parameters
             var choices = await database.GetPlayerChoiceResponsesAsync();
             if (choices != null)
                 foreach (var item in choices)
-                    Items.Add(item.ResponseId, new SelectOption(item.Answer));
+                    Items.Add(item.ResponseId, new SelectOption(string.IsNullOrEmpty(item.Header) ? item.Answer : item.Header));
         }
     }
     
