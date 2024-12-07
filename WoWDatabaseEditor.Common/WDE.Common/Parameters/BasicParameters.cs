759636a4f9f2d4e46d6e914952a14152923cebbb
Higi <higi@tauri.hu>
2024-12-07 20:09:09 +0100

    Enum types now display key with name

--- WoWDatabaseEditor.Common/WDE.Common/Parameters/BasicParameters.cs
+++ WoWDatabaseEditor.Common/WDE.Common/Parameters/BasicParameters.cs
@@ -12,7 +12,7 @@ namespace WDE.Common.Parameters
         public override string ToString(long key)
         {
             if (Items != null && Items.TryGetValue(key, out var option))
-                return option.Name;// + " (" + key + ")";
+                return option.Name + " (" + key + ")";
             return key.ToString();
         }
 
