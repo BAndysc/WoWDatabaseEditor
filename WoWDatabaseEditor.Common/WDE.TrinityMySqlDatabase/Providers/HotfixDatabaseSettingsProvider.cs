233b1437b65254ebedac3e0b753b5754333496de
Higi <higi@tauri.hu>
2025-11-28 15:13:04 +0100

    fml.cfg

--- WoWDatabaseEditor.Common/WDE.TrinityMySqlDatabase/Providers/HotfixDatabaseSettingsProvider.cs
+++ WoWDatabaseEditor.Common/WDE.TrinityMySqlDatabase/Providers/HotfixDatabaseSettingsProvider.cs
@@ -16,7 +16,7 @@ public class HotfixDatabaseSettingsProvider : IHotfixDatabaseSettingsProvider, I
         this.userSettings = userSettings;
     }
 
-    public string ConnectionString => $"Server={Settings.Host};Port={Settings.Port ?? 3306};Database={Settings.Database};Uid={Settings.User};Pwd=\"{Settings.Password}\";AllowUserVariables=True;TreatTinyAsBoolean=False"";
+    public string ConnectionString => $"Server={Settings.Host};Port={Settings.Port ?? 3306};Database={Settings.Database};Uid={Settings.User};Pwd=\"{Settings.Password}\";AllowUserVariables=True;TreatTinyAsBoolean=False";
     public string DatabaseName => Settings.Database ?? "";
     public bool IsEmpty => string.IsNullOrEmpty(Settings.Host) || string.IsNullOrEmpty(Settings.Database) || string.IsNullOrEmpty(Settings.User);
 
